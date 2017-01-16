3ce27b017ee2d28a5358e155c97081a6b2af11b9
Nam <nguyenvotiendung2014@gmail.com>
2017-01-16 18:56:55 +0700

    admin website dr.vuong

---
+++ app/Http/Controllers/admin_controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB; //thu vien du lieu
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+
+use App\Http\Requests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Session;
+
+class admin_controller extends Controller
+{
+    //
+    public function welcome()
+    {
+        return view("pages.trangchao");
+    }
+}
